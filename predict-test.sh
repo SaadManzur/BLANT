29c3c6ae96cfc59e8018f64f32dc3d1a97530a78
Wayne Hayes <whayes@uci.edu>
2021-01-11 22:18:42 -0800

    ensure nodes printed in MAX,MIN order

--- predict-test.sh
+++ predict-test.sh
@@ -8,7 +8,7 @@ do
 	time ./predict-edges-from-network.sh -B ./blant.test -M $M ~/extra1/preserve/Kovacs/HI-union/split-by-edges/F0/HI-union-train0.el $K |
 	    sed 's/:/    /' |
 	    head -$TOP |
-	    awk '{ printf "%s\t%s\n%s\t%s\n", $1,$2,$2,$1 }' |
+	    hawk '{ printf "%s\t%s\n", MAX($1,$2),MIN($1,$2)}' |
 	    fgrep -c -f - ~/extra1/preserve/Kovacs/HI-union/split-by-edges/F0/HI-union-test0.el |
 	    awk '{printf "\t\t\t**********************\tM='$M' K='"$K"': Recovered %d edges out of '$TOP', precision %g%% ********************\n",
 		$1,100*$1/'$TOP'}'
